9925b245f8b80d0ce81a04c9ed811697fe80f793
Zero <alengebles@gmail.com>
2024-07-03 21:59:31 +0200

    added a third section

---
+++ app/components/PageThree/page.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import './pageThree.css';
+import Link from 'next/link';
+
+function PageThree() {
+  const project = ({ imgSrc, projectName, projectSubject, year }) => {
+    return (
+      <Link href="#">
+        <div className="project">
+            <img className="projectImage" src={imgSrc} alt={projectName} />
+          <div className="projectInfo">
+            <div className='info'>
+              <h3>{projectName}</h3>
+              <p>{projectSubject}</p>
+            </div>
+            <p className='year'>{year}</p>
+          </div>
+        </div>
+      </Link>
+    );
+  };
+
+  const projectData1 = {imgSrc: '/landingImages/image1.webp', projectName: 'Recket', projectSubject: 'Recycling website', year: 'Highlighted',};
+  const projectData2 = {imgSrc: '/landingImages/image2.webp', projectName: 'Alexander William', projectSubject: 'Private Investments', year: '2023',};
+  const projectData3 = {imgSrc: '/landingImages/image3.webp', projectName: 'Ultra', projectSubject: 'Online Bank', year: '2024',};
+  const projectData4 = {imgSrc: '/landingImages/image4.webp', projectName: 'Dorian Eskola', projectSubject: 'Fitness website', year: '2024',};
+
+  return (
+    <section className="pageThree">
+      <h1 className="featuredText">Featured work</h1>
+      {project(projectData1)}
+      {project(projectData2)}
+      {project(projectData3)}
+      {project(projectData4)}
+    </section>
+  );
+}
+
+export default PageThree;
--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import LandingPage from "./components/Landing-page/page";
 import PageTwo from "./components/PageTwo/page";
+import PageThree from "./components/PageThree/page";
 
 export default function Home() {
   return (
     <main>
       <LandingPage />
       <PageTwo />
+      <PageThree />
     </main>
   );
 }

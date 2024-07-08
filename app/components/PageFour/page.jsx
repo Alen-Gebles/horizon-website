ba3c95dbe3cf78c0a03616610af2840bacd3965e
Zero <alengebles@gmail.com>
2024-07-08 11:20:11 +0200

    added section 4

--- app/components/PageFour/page.jsx
+++ app/components/PageFour/page.jsx
@@ -1,13 +1,37 @@
 "use client"
-import React from "react";
-import { useState, useEffect, useRef } from 'react';
+import React, { useState } from "react";
 import "./pageFour.css";
+import data from '../../data/data.json';
 
 function PageFour() {
+  const [buttonValue, setButtonValue] = useState('branding');
 
+  const handleButtonClick = (value) => {
+    setButtonValue(value);
+  };
 
   return (
     <section className="pageFour">
+      <h2 className="FourHeadline">Select the full range of services<br />or simply the ones you prefer...</h2>
+      <div className="serviceBox">
+        <div className="fourButtonBox">
+          <button className={`serviceButton ${buttonValue === 'branding' ? 'text-white' : ''}`} onClick={() => handleButtonClick('branding')}>Branding</button>
+          <button className={`serviceButton ${buttonValue === 'design' ? 'text-white' : ''}`} onClick={() => handleButtonClick('design')}>Web Design</button>
+          <button className={`serviceButton ${buttonValue === 'development' ? 'text-white' : ''}`} onClick={() => handleButtonClick('development')}>Web Development</button>
+          <button className={`serviceButton ${buttonValue === 'product' ? 'text-white' : ''}`} onClick={() => handleButtonClick('product')}>Product Overview</button>
+        </div>
+        <div className={`fourTextBox `}>
+          {data[buttonValue].map(item => (
+            <div className="textBox" key={item.id}>
+              <div className="w-full flex justify-between items-center mb-3">
+                <h3 className="titleFour">{item.title}</h3>
+                <p className="time">{item.time}</p>
+              </div>
+              <p className="fourText">{item.text}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
   );
 }
